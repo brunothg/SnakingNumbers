16e9f0f096a414949758bd408310a8abe65e1f8a
brunothg <android.bruns@googlemail.com>
2014-10-09 22:09:00 +0200

    import cleaning

--- app/src/main/java/de/bno/snakingnumbers/result/GameResult.java
+++ app/src/main/java/de/bno/snakingnumbers/result/GameResult.java
@@ -18,12 +18,8 @@
 
 package de.bno.snakingnumbers.result;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
